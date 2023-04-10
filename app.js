b3653a6f3e7f4904f1468badac25d758b9c09d01
EmFey <manufety15@gmail.com>
2023-04-10 15:36:28 +0200

    Created module to display to html

--- app.js
+++ app.js
@@ -1,21 +1,112 @@
-const Gameboard = (() => {
-  // Gameboard state
-  let board = ['', '', '', '', '', '', '', '', ''];
-
-  // Public methods
-  const getBoard = () => board;
-
-  const updateBoard = (index, marker) => {
-    board[index] = marker;
-  };
+// Gameboard module
+const gameBoard = (() => {
+  const board = ['', '', '', '', '', '', '', '', ''];
 
   const resetBoard = () => {
-    board = ['', '', '', '', '', '', '', '', ''];
+    for (let i = 0; i < board.length; i++) {
+      board[i] = '';
+    }
+  };
+
+  const getBoard = () => board;
+
+  const makeMove = (index, player) => {
+    board[index] = player.getSymbol();
   };
 
   return {
-    getBoard,
-    updateBoard,
     resetBoard,
+    getBoard,
+    makeMove,
   };
 })();
+
+// Player factory function
+const Player = (name, symbol) => {
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  return { getName, getSymbol };
+};
+
+// Game module
+const game = (() => {
+  let player1, player2, currentPlayer;
+  let gameOver = false;
+
+  const initializeGame = (name1, symbol1, name2, symbol2) => {
+    player1 = Player(name1, symbol1);
+    player2 = Player(name2, symbol2);
+    currentPlayer = player1;
+    gameBoard.resetBoard();
+    gameOver = false;
+    displayController.updateBoard(gameBoard.getBoard());
+  };
+
+  const switchPlayer = () => {
+    if (currentPlayer === player1) {
+      currentPlayer = player2;
+    } else {
+      currentPlayer = player1;
+    }
+  };
+
+  const playTurn = (index) => {
+    if (!gameOver && gameBoard.getBoard()[index] === '') {
+      gameBoard.makeMove(index, currentPlayer);
+      displayController.updateBoard(gameBoard.getBoard());
+      checkWin();
+      switchPlayer();
+    }
+  };
+
+  const checkWin = () => {
+    const board = gameBoard.getBoard();
+    const winPatterns = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal wins
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical wins
+      [0, 4, 8], [2, 4, 6] // diagonal wins
+    ];
+    for (let i = 0; i < winPatterns.length; i++) {
+      const [a, b, c] = winPatterns[i];
+      if (board[a] !== '' && board[a] === board[b] && board[a] === board[c]) {
+        displayController.displayWin(currentPlayer.getName());
+        gameOver = true;
+        return;
+      }
+    }
+    if (!board.includes('')) {
+      displayController.displayDraw();
+      gameOver = true;
+    }
+  };
+
+  return { initializeGame, playTurn };
+})();
+
+const displayController = (() => {
+  // Select the game board element and its cells
+  const form = document.querySelector('.cellGrid');
+  const cells = Array.from(document.querySelectorAll('.field'));
+  const message = document.querySelector('.message');
+  const resetBtn = document.querySelector('.btnRestart');
+
+  // Render the current game board state on the UI
+  function renderBoard(gameboard) {
+    cells.forEach((cell, index) => {
+      cell.textContent = gameboard[index];
+    });
+  }
+
+  // Clear the game board on the UI
+  function clearBoard() {
+    cells.forEach((cell) => {
+      cell.textContent = '';
+    });
+  }
+
+  return {
+    renderBoard,
+    clearBoard,
+  };
+})();
+
