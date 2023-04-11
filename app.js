37d5b2c809755a764178b364d967ad00aeffb0c3
EmFey <manufety15@gmail.com>
2023-04-11 14:59:10 +0200

    Simplified the JS code

--- app.js
+++ app.js
@@ -1,144 +1,91 @@
-// Gameboard module
-const gameBoard = (() => {
-  let gameArray = ["", "", "", "", "", "", "", "", ""];
-
-  const getGameArray = () => gameArray;
-  const updateGameArray = (index, value) => {
-    gameArray[index] = value;
-  };
-  const resetGameArray = () => {
-    gameArray = ["", "", "", "", "", "", "", "", ""];
-  };
-
-  return {
-    getGameArray,
-    updateGameArray,
-    resetGameArray,
-  };
-})();
-
-
-
-// Player factory function
-const Player = (name, symbol) => {
-  const getName = () => name;
-  const getSymbol = () => symbol;
-
-  return { getName, getSymbol };
+// Factory function for creating players
+const createPlayer = (name, marker) => {
+  return { name, marker };
 };
 
-
-
-// Game flow module
+// Module for managing game flow
 const gameFlow = (() => {
-  const player1 = Player("Player 1", "X");
-  const player2 = Player("Player 2", "O");
-  let currentPlayer = player1;
+  let currentPlayer;
+  let gameBoard;
 
-  const gameTurn = (e) => {
-    const gameArray = gameBoard.getGameArray();
-    const index = e.target.dataset.index;
+  const startGame = () => {
+    gameBoard = ["", "", "", "", "", "", "", "", ""];
+    currentPlayer = player1;
+    displayController.render(gameBoard);
+  };
 
-    if (gameArray[index] !== "") return;
-
-    gameBoard.updateGameArray(index, currentPlayer.getSymbol());
-    displayController.renderBoard();
-
-    if (checkWin(currentPlayer.getSymbol())) {
-      displayController.showMessage(`${currentPlayer.getName()} wins!`);
-      displayController.disableBoard();
-      return;
-    }
-
-    if (checkDraw()) {
-      displayController.showMessage("Draw!");
-      displayController.disableBoard();
-      return;
-    }
-
+  const nextTurn = () => {
     currentPlayer = currentPlayer === player1 ? player2 : player1;
   };
 
-  const checkWin = (symbol) => {
-    const gameArray = gameBoard.getGameArray();
-    const winCombinations = [
+  const playTurn = (index) => {
+    if (gameBoard[index] === "") {
+      gameBoard[index] = currentPlayer.marker;
+      nextTurn();
+      displayController.render(gameBoard);
+      checkWinner();
+    }
+  };
+
+  const checkWinner = () => {
+    const winConditions = [
+      // Rows
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
+      // Columns
       [0, 3, 6],
       [1, 4, 7],
       [2, 5, 8],
+      // Diagonals
       [0, 4, 8],
       [2, 4, 6],
     ];
 
-    return winCombinations.some((combination) => {
-      return combination.every((index) => gameArray[index] === symbol);
-    });
-  };
-
-  const checkDraw = () => {
-    const gameArray = gameBoard.getGameArray();
-    return gameArray.every((value) => value !== "");
-  };
-
-  function resetGame() {
-    // reset game board array and render empty grid
-    gameBoard.resetBoard();
-    displayController.render(gameBoard.getBoard());
-  
-    // reset game status message
-    displayController.updateMessage("Player 1's turn");
-  
-    // enable grid clicks and add event listener to each grid
-    const grids = document.querySelectorAll('.field');
-    for (let i = 0; i < grids.length; i++) {
-      grids[i].addEventListener('click', playTurn);
-      grids[i].classList.remove('x');
-      grids[i].classList.remove('o');
+    for (let i = 0; i < winConditions.length; i++) {
+      const [a, b, c] = winConditions[i];
+      if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+        displayController.showWinner(currentPlayer);
+        return;
+      }
     }
-  }
-  
-  // event listener for reset button click
-  document.getElementById('reset-button').addEventListener('click', resetGame);
-});
 
-
-
-// Display controller module
-const displayController = (() => {
-  const field = document.querySelectorAll(".field");
-  const message = document.querySelector(".message");
-  const resetButton = document.querySelector(".restartBtn");
-
-  const renderBoard = () => {
-    const gameArray = gameBoard.getGameArray();
-    field.forEach((div, index) => {
-      div.textContent = gameArray[index];
-    });
+    if (!gameBoard.includes("")) {
+      displayController.showTie();
+    }
   };
 
-  const showMessage = (message) => {
-    message.textContent = message;
-  };
-
-  const clearMessage = () => {
-    message.textContent = "";
-  };
-
-  const disableBoard = () => {
-    field.forEach((div) => {
-      div.removeEventListener("click", gameFlow.gameTurn);
-    });
-  };
-
-  const enableBoard = () => {
-    field.forEach((div) => {
-      div.addEventListener("click", gameFlow.gameTurn);
-    });
-  };
-
-  resetButton.addEventListener("click", gameFlow.resetGame);
-
-  return { renderBoard, showMessage, clearMessage, disableBoard, enableBoard };
+  return { startGame, playTurn };
 })();
+
+// Module for rendering game board and messages to the screen
+const displayController = (() => {
+  const board = document.querySelector(".board");
+  const message = document.querySelector(".message");
+
+  const render = (gameBoard) => {
+    board.innerHTML = "";
+    gameBoard.forEach((cell, index) => {
+      const div = document.createElement("div");
+      div.classList.add("field");
+      div.textContent = cell;
+      div.addEventListener("click", () => gameFlow.playTurn(index));
+      board.appendChild(div);
+    });
+  };
+
+  const showWinner = (player) => {
+    message.textContent = `${player.name} wins!`;
+  };
+
+  const showTie = () => {
+    message.textContent = "It's a tie!";
+  };
+
+  return { render, showWinner, showTie };
+})();
+
+// Create players and start game
+const player1 = createPlayer("Player 1", "X");
+const player2 = createPlayer("Player 2", "O");
+gameFlow.startGame();
